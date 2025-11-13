c1d0468bf471f2362d4c2156a6399e5ffcf4bba3
RewantRawat <rewantrawat1596@gmail.com>
2025-11-13 10:35:14 +0530

    first commit

---
+++ Basics/variables.js
@@ -0,0 +1,27 @@
+const accountId = 12345
+let accountEmail="rewant@google.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountDeatils;
+
+accountEmail = "hfhf@gmail.com"
+accountPassword="12345543"
+accountCity="Pune"
+
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountDeatils])
+
+var x =10
+x=20 //reassigned value
+console.log(x);
+
+let y=5
+y=15//reassigned
+console.log(y);
+
+const z=30;
+// z=20 //reassigned error in const
+console.log(z);
+
+var x = 10;
+var x = 20; // allowed
+console.log(x); 

a1fa1f432d1073e517f82c12c5917c3554ec9732
RewantRawat <rewantrawat1596@gmail.com>
2025-11-17 09:45:01 +0530

    commit array files

---
+++ Basics/Arrays.js
@@ -0,0 +1,127 @@
+// const name = "khushi"
+// const age = "23"
+
+// console.log(`hello ${name},my age is ${age}`)
+// console.log(" hello " + name + "my age is"+age);
+
+// //datatypes
+
+// //primitive datatypes
+// // Number string boolean Symbol, bignint undefined
+
+// // //reference
+
+// // array objects functions
+
+
+// const marks = [97,55,67,89,76,66]
+// console.log(marks);
+// console.log(marks.length);
+
+
+const heros = ["thor","hulk","vm","super"]
+let cities = ["jaipur","panipat","delhi","guragaon","pune"]
+// console.log(heros[2]);
+
+for(let i=0;i<heros.length;i++){
+    console.log(heros[i]);
+    }
+
+    for(let city of cities){
+        // console.log(city);
+         console.log(city.toUpperCase());
+        
+    }
+
+    let marks = [85,97,54,37,76,60]
+
+    let sum =0
+
+    for(let val of marks){
+        sum += val
+    }
+
+    let average=sum/marks.length
+    console.log(`Average marks of students of class ${average}`);
+    
+// for(let hero of heros){
+//     console.log(hero);
+    
+// }
+let arr = [50,50,50,50,50]
+let sum1 = 0
+
+for(let value of arr){
+    sum1 += value
+}
+
+let avg = sum1/arr.length
+console.log(avg);
+
+
+
+// const sum1 = 0
+
+// for(let ar of arr){
+//     sum1 += ar
+// }
+
+// console.log(sum1);
+
+//ARRAY METHOS//
+
+//1.POP=REMOVE ELEMNT FROM END
+//2.PUSH=ADD TO END
+//3.TOSTRING=CONVERT ARRAY IN STRING
+//4.CONCAT=JOINS MULTIPLE ARAYS & RETURN RESULT
+//5.UNSHIFT=ADD TO START
+//6.SHIFT=DELETE FROM START & RETURN
+//7.SLICE=RETURNS THE PEICE OF ARRAY
+//8.SPLICE=CHANGE ORIGINAL ARRAY(ADD,REMOVE,REPLACE)
+
+const foodItems  = ["apple","potato","litchi"]
+
+foodItems.push("guavava","roots")
+
+console.log(foodItems);
+console.log(foodItems.toString());
+const deletedItems = foodItems.pop()
+console.log(deletedItems);
+const deletedItems1 = foodItems.pop()
+console.log(deletedItems1)
+
+
+let marvel_heros = ["thor","spiderman","ironamn","spiderman","dr strange"]
+console.log(marvel_heros);
+
+console.log(marvel_heros.slice(1,3));
+
+let array = [1,2,3,4,5,6,7]
+console.log(array.splice(2,4));
+// console.log(array.splice(2));
+console.log(array);
+
+//PRACTICE
+const companies = ["Bloomberg","microsoft","Uber","Google","IBM","Netflix"]
+// console.log(companies.shift());
+// console.log(companies);
+// console.log(companies.push("Amazon
+// console.log(companies);
+
+console.log(companies.splice(2,1,"OLA"));
+console.log(companies);
+
+
+
+
+
+
+// const val = marvel_heros.shift()
+
+// console.log("deleted items",val);
+
+
+// let dc_heros = ["superman","batman"]
+// let indian_heros=["shaktiman","krish"]
+// let heross = marvel_heros.concat(dc_heros,indian_heros)
+// console.log(heross);
--- Basics/Practice.js
+++ Basics/Practice.js
@@ -4,41 +4,44 @@
 //Paramter string return string
 export function sort(name) {
     //Method 1
-    return name.split("").sort().join('')
+    // return name.split("").sort().join('')
 
-    //Method
-    // let arr = []
+    // Method
+    let arr = []
 
-    // for (let i = 0; i < name.length; i++) {
-    //     arr[arr.length] = name[i]
-    // }
-    
-    // for (let i = 0; i < arr.length; i++) {
-    //     for (let j = 0; j < arr.length; j++) {
-    //         if (arr[j] > arr[j + 1]) {
-    //             let temp = arr[j]
-    //             arr[j] = arr[j + 1]
-    //             arr[j + 1] = temp
-    //         }
-    //     }
-    // }
-    // let result = ""
-    // for (let i = 0; i < arr.length; i++) {
-    //     result += arr[i]
-    // }
-    // return result
+    for (let i = 0; i < name.length; i++) {
+        arr[arr.length] = name[i]
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+                console.log("ff", temp);
+            }
+        }
+    }
+
+
+    let result = ""
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i]
+    }
+    return result
 }
 
 //Paramter string return string
 export function reverse(name) {
 
-//Method 1
+    //Method 1
     // return name.split('').reverse().join('')
 
- //Method 2   
+    //Method 2   
     let result = []
-    for(let i =name.length-1;i>=0;i--){
-        result +=name[i]
+    for (let i = name.length - 1; i >= 0; i--) {
+        result += name[i]
     }
     return result
 }
